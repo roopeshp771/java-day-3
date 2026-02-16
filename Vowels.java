7ac39d305dec19518678f36fc5f4e2a2652583eb
PULLIGANI ROOPESH <roopeshp771@gmail.com>
2026-02-16 12:12:44 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,17 @@
+public class Vowels
+{
+    public static void main(String[] args)
+    {
+        String str="interview";
+        int count=0;
+        for(int i=0;i<str.length();i++)
+        {
+            char ch=str.charAt(i);
+            if(ch == 'a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+            {
+                count++;
+            }
+        }
+    System.out.println("the vowel count:"+count);
+    }
+}
